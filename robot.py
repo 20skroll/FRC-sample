fa07171eaac5d7546ba436206e71b9c29274634d
20skroll <20skroll@pierz.k12.mn.us>
2019-09-19 04:27:03 +0000

    Borrowed example code

--- robot.py
+++ robot.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+import wpilib
+
+
+class MyRobot(wpilib.IterativeRobot):
+    def robotInit(self):
+        """Robot initialization function"""
+
+        # object that handles basic drive operations
+        self.myRobot = wpilib.RobotDrive(0, 1)
+        self.myRobot.setExpiration(0.1)
+
+        # joystick #0
+        self.stick = wpilib.Joystick(0)
+
+    def teleopInit(self):
+        """Executed at the start of teleop mode"""
+        self.myRobot.setSafetyEnabled(True)
+
+    def teleopPeriodic(self):
+        """Runs the motors with tank steering"""
+        self.myRobot.arcadeDrive(self.stick, True)
+
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
